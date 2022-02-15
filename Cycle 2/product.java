4d4cf5fa72d5d68cac309a7de1cdd17b26e6a8a8
Devakrishna <68635645+Devadk13@users.noreply.github.com>
2022-02-15 13:38:39 +0530

    Add files via upload

---
+++ Cycle 2/product.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class product
+{
+public static void main(String args[])
+{
+
+
+
+
+Scanner obj=new Scanner(System.in);
+
+int a,b, p;
+System.out.println("Enter a");
+a= obj.nextInt();
+System.out.println("enter b");
+b= obj.nextInt();
+
+p=a*b;
+
+System.out.println(p); 
+
+}
+}
