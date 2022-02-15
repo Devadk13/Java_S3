acf6528ccd8235ab4dd277305ddd6c5c687e3343
Devakrishna <68635645+Devadk13@users.noreply.github.com>
2022-02-15 13:46:43 +0530

    Update exp1.java

--- Cycle 5/exp1.java
+++ Cycle 5/exp1.java
@@ -1,1 +1,19 @@
-j
+import java.io.*;
+import java.util.*;
+class experiment1
+{
+    public static void main(String[] args) throws Exception
+    {
+    FileInputStream fin = new FileInputStream("e1.txt");
+    int i=1;
+    int ch;
+    System.out.print(i+"-");
+    while((ch = fin.read())!=-1)
+    {
+    System.out.print((char)ch);
+     if(ch=='\n')
+        System.out.print(++i+"-");
+    }
+    fin.close();
+    }
+}
