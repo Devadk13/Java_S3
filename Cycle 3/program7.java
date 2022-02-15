022e0b766553fb09b24f53dfcb82f50da57c0419
Devakrishna <68635645+Devadk13@users.noreply.github.com>
2022-02-15 08:07:53 +0530

    Create program7.java

---
+++ Cycle 3/program7.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class employee 
+{
+    String name ;
+    int empno;
+    long pn;
+    void EnterDetails()  
+      {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter Name :");
+        name = obj.nextLine();  
+       
+        System.out.print("Enter Empo No :"); 
+        empno = obj.nextInt();
+        
+        System.out.print("Enter Phone no :"); 
+        pn = obj.nextLong();
+      }
+       
+    void PrintDetails()
+      {
+        
+        System.out.println("Name : "+name);
+        
+        System.out.println("Empo No : "+empno); 
+
+        System.out.println("Phone no : "+pn); 
+      }
+
+    public static void main(String args[]) 
+      {
+        int i;  
+        employee obj[] = new employee[50];
+
+        for(i=0;i<5;i++) 
+          {  
+            obj[i] = new employee();
+            obj[i].EnterDetails(); 
+          }
+          System.out.println("Details");
+          for(i=0;i<5;i++) 
+          {
+            obj[i].PrintDetails();
+          }
+       }
+}
